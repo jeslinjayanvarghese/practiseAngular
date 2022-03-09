9fb83c76d8edeabeac6a326ca50103427b364bef
AzureAD\JeslinJayanVarghese <jeslin.varghese@skywalkglobal.net>
2022-03-09 17:31:37 +0530

    routing done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,31 +3,51 @@ import { DbService } from './services/db/db.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-
-import { MessagesModule } from './messages/messages.module';
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MessageService } from './message.service';
 import { CookieService } from 'ngx-cookie-service';
 
 
-
+import { CommonModule } from '@angular/common';
+import { MessagesComponent} from './messages/messages.component'
+import { InputComponent } from './input/input.component';
+import { ViewmsgComponent } from './viewmsg/viewmsg.component';
+import { ClockComponent } from './clock/clock.component';
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule  } from '@angular/material/sidenav';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule} from '@angular/material/icon';
+import { MatDividerModule} from '@angular/material/divider';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    ViewmsgComponent,
+    MessagesComponent,
+    InputComponent,
+    ClockComponent
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
- 
-    MessagesModule
+    CommonModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    HttpClientModule,
+    FormsModule,
   ],
   exports: [],
   providers: [MessageService,DbService,CookieService],
---
+++ src/app/clock/clock.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+import * as momenttz from 'moment-timezone';
+
+@Component({
+  selector: 'app-clock',
+  templateUrl: './clock.component.html',
+  styleUrls: ['./clock.component.css']
+})
+export class ClockComponent implements OnInit {
+
+  constructor() { }
+  // todayDate:any = moment().format('M/D/YYYY');
+  // todayDate = new Date();
+  local:any='';
+  ist:any=';'
+  uk:any='';
+  ngOnInit(): void {
+    this.getLocalTime();
+  }
+  getLocalTime(){
+    this.local = moment().format('MMMM Do YYYY, h:mm:ss a')
+    console.log(this.local);
+    this.ist = momenttz().tz("Asia/Calcutta").format('MMMM Do YYYY, h:mm:ss a');
+    console.log(this.ist);
+    this.uk =momenttz().tz("Europe/London").format('MMMM Do YYYY, h:mm:ss a');
+    console.log(this.uk);
+  }
+
+}
